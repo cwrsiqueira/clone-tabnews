3e246f6060cd06566028466ecac3c7721546fe6e
Carlos Wagner <44854098+cwrsiqueira@users.noreply.github.com>
2024-04-01 23:48:55 +0000

    alterado o texto

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ function Home(){
     return (
         <div>
             <h2>Carlos Eduardo,</h2>
-            <h1>papai te ama <span style={{color:'red'}}>&hearts; &hearts; &hearts;</span></h1>
+            <h1>papai te ama</h1>
+            <span style={{color:'red'}}>&hearts; &hearts; &hearts;</span>
         </div>
     )
 }
