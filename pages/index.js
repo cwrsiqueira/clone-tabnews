0afb87a348518a25e1a96f5a97f66767b460b5dd
Carlos Wagner <44854098+cwrsiqueira@users.noreply.github.com>
2024-04-01 23:53:51 +0000

    Erro

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 function Home(){
     return (
-        <div>
-            <h2>Carlos Eduardo,</h2>
-            <h1>papai te ama</h1>
-            <span style={{color:'red'}}>&hearts; &hearts; &hearts;</span>
-        </div>
+        <h1>Eduardo, papai te ama! Muito!!!</h1>
     )
 }
 
