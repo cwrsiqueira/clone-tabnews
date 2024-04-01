fde6c1cd1421eb3b85c11523ab5b50de22dd9cce
Carlos Wagner <44854098+cwrsiqueira@users.noreply.github.com>
2024-04-01 23:29:22 +0000

    adiciona pages

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+    return (
+        <div>
+            <h2>Carlos Eduardo,</h2>
+            <h1>papai te ama <span style={{color:'red'}}>&hearts; &hearts; &hearts;</span></h1>
+        </div>
+    )
+}
+
+export default Home;
